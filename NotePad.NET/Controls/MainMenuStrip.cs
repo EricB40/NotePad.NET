ace5f249ae842b28a2f7b709e4e2640bb4d96196
EricB40 <barbereaue@gmail.com>
2023-05-28 22:05:59 +0200

    feat: Name and Dock Properties of MainMenuStrip class

--- NotePad.NET/Controls/MainMenuStrip.cs
+++ NotePad.NET/Controls/MainMenuStrip.cs
@@ -1,6 +1,14 @@
-﻿namespace NotePad.NET.Controls
+﻿using System.Windows.Forms;
+
+namespace NotePad.NET.Controls
 {
-    internal class MainMenuStrip
+    internal class MainMenuStrip : MenuStrip
     {
+        public MainMenuStrip()
+        {
+            Name = "MainMenuStrip";
+            /* Since it's inheriting from MenuStrip we can acces and filter all properties through Intellisense.*/
+
+        }
     }
 }
--- README.txt
+++ README.txt
@@ -7,3 +7,7 @@ Workflow:
 - MainForm Property Text to NotePad.NET
 - Added new folder Controls to the project
 - Added new class MainMenuStrip.cs in folder Controls
+- This class inherits for MenuStrip from System.Windows.Forms
+- MainMenuStrip Constructor
+		- Name property (Name="MainMenuStrip")
+		- Dock Property to Top (Dock = DockStyle.Top)
